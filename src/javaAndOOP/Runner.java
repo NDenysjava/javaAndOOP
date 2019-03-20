9183824aff49593a14985305f5e7b3d440c83d2b
DENYA <DENYA@DESKTOP-GRPT1FO>
2019-03-20 22:36:50 +0200

    Add logger

--- src/javaAndOOP/Runner.java
+++ src/javaAndOOP/Runner.java
@@ -1,24 +1,27 @@
 package javaAndOOP;
+import java.util.logging.Logger;
+
 import javaAndOOP.circle.Circle;
 import javaAndOOP.square.*;
 import javaAndOOP.triangle.*;
 
-@SuppressWarnings("unused")
 public class Runner {
 
+	private static Logger logger = Logger.getLogger(Runner.class.getName());
+	
 	public static void main(String[] args) {
 		Circle circle = new Circle();
 		circle.setRadius(5);
-		System.out.println("the area of the circle is " + circle.getArea());
+		logger.info("the area of the circle is " + circle.getArea());
 		
 		Square square = new Square();
 		square.setSide(2.5);
-		System.out.println("the area of ​​the square is " + square.getArea());
+		logger.info("the area of ​​the square is " + square.getArea());
 		
 		Triangle triangle = new Triangle();
 		triangle.setWidth(28);
 		triangle.setHeight(12.6);
-		System.out.println("the area of ​​the triangle is equal to " + triangle.getArea());
+		logger.warning("the area of ​​the triangle is equal to " + triangle.getArea());
 		
 		
 		
